74d1929297bab86867b95a66b0c2cbc34efe6752
Dumith <dumith98@gmail.com>
2021-07-19 21:07:43 -0300

    Able to find card with multiple search results
    The scraper can successfully find the desired card when searched

--- CardScraper.py
+++ CardScraper.py
@@ -5,8 +5,8 @@ from bs4 import BeautifulSoup
 
 nav = webdriver.Chrome()
 
-card_name = input('Insert the name of the desired card\'s name: ')
-
+card= input('Insert the name of the desired card\'s name: ')
+card_name = card.title()
 
 nav.get('https://gatherer.wizards.com/Pages/Default.aspx')
 
@@ -22,7 +22,6 @@ for parent in desired_card.parents:
     if parent.name == 'a':
         card_url = str(parent['href'])
         card_url = card_url.replace('..', '')
-
 treatred_card_url = 'https://gatherer.wizards.com/Pages' + card_url
 print(treatred_card_url)
 nav.get(treatred_card_url)
