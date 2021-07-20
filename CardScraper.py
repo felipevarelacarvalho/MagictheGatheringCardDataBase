6706e8b17e9ba8f8a03086db1e5354402c33919a
Dumith <dumith98@gmail.com>
2021-07-19 21:28:34 -0300

    Seek and Found Complete; Still need to treat misspelling erros
    The script now can find cards if it has many results or only one.  No workaround misspelling errors.

--- CardScraper.py
+++ CardScraper.py
@@ -12,16 +12,18 @@ nav.get('https://gatherer.wizards.com/Pages/Default.aspx')
 
 nav.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_Content_SearchControls_CardSearchBoxParent_CardSearchBox"]').send_keys(card_name)
 nav.find_element_by_xpath('//*[@id="ctl00_ctl00_MainContent_Content_SearchControls_CardSearchBoxParent_CardSearchBox"]').send_keys(Keys.ENTER)
-
 r = requests.get(nav.current_url).text
 soup = BeautifulSoup(r, 'html.parser')
 
-desired_card = soup.find('img', {'alt': card_name })
+if soup.find_all('div', {'class':'cardContent'}):
+    print('Found you card!')
 
-for parent in desired_card.parents:
-    if parent.name == 'a':
-        card_url = str(parent['href'])
-        card_url = card_url.replace('..', '')
-treatred_card_url = 'https://gatherer.wizards.com/Pages' + card_url
-print(treatred_card_url)
-nav.get(treatred_card_url)
+else:
+    desired_card = soup.find('img', {'alt': card_name })
+    for parent in desired_card.parents:
+        if parent.name == 'a':
+            card_url = str(parent['href'])
+            card_url = card_url.replace('..', '')
+    treatred_card_url = 'https://gatherer.wizards.com/Pages' + card_url
+    print('Found it among many search results!')
+    nav.get(treatred_card_url)
